16794ec3893500360ee7c1087d927f52dbe36639
max77p <shanindrakumar@gmail.com>
2020-08-04 16:27:45 -0400

    my changes

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
   version = "~> 2.0"
-  region  = "eu-central-1"
+  region  = "ca-central-1"
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "example-terraform-project-name-bootstrap-terraform-state"
-#     key    = "default-infrastructure"
-#     region = "eu-central-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "si-remote-state-bucket"
+    key    = "default-infrastructure"
+    region = "ca-central-1"
+  }
+}
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "example-terraform-project-name-bootstrap-terraform-state"
+  bucket = "si-remote-state-bucket"
 
   versioning {
     enabled = true
